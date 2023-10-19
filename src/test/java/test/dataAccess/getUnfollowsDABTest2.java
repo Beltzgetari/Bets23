3150df833b553fe35a8e2fd047760b7951f6bf07
unax8 <unax8@PCira>
2023-10-19 19:39:31 +0200

    Iraola Proba aldaketak

---
+++ src/test/java/test/dataAccess/getUnfollowsDABTest2.java
@@ -0,0 +1,79 @@
+package test.dataAccess;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import configuration.ConfigXML;
+import dataAccess.DataAccess;
+import domain.User;
+import domain.UserAbstract;
+public class getUnfollowsDABTest2 {
+	private ArrayList<User> unfollows ;
+	private DataAccess da;
+	private User Mikel2;
+	private User nulo;
+	private User Xabi;
+	private User mikelezdatubasean;
+	private User mikel;
+	
+	@Before
+	public void initialize() {
+		Mikel2 = new User("Beltzgetari2", "OnePieceUnaMierdax2", "Mikel2", 19 );
+		
+		
+		da = new DataAccess();
+		da.ezabatu();
+		mikel = da.getUserUsername("Beltzgetari");
+		nulo = null;
+		mikelezdatubasean = da.getUserUsername("Mikel-Xabi");
+		Xabi = new User("MundukoErregie", "OnePieceUnaMierda", "Xabi", 20);
+		
+		
+		da.register(Xabi.getUsername(), Xabi.getPassword(), Xabi.getIzena(), Xabi.getAge());
+		da.register(Mikel2.getUsername(), Mikel2.getPassword(), Mikel2.getIzena(), Mikel2.getAge());
+	}
+	
+	@Test
+	public void Test1() {
+		try{
+			da.getUnfollows(nulo);
+		
+		
+		}catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+	@Test
+	public void Test2() {
+		try{
+			da.getUnfollows(mikelezdatubasean);
+		
+		
+		}catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+	/**
+	 * 
+	 * @Test
+	public void Test3() { 
+		ArrayList<User> xabibakarrik = new ArrayList<User>();
+		
+		xabibakarrik.add(Xabi);
+	ArrayList<User> listaemaitza = da.getUnfollows(Mikel2);
+		for ( int  a =0; a <listaemaitza.size(); a++) {
+			System.out.println(	listaemaitza.size());
+			
+			assertTrue(xabibakarrik.get(a).getUsername().equals(listaemaitza.get(a).getUsername()));
+		}
+	}
+	 */
+	
+	
+}
